703dadf5ccc82d5f494ade38c7717e54448ced3b
dharunkumar <kumardharun607@gmail.com>
2026-01-07 15:48:25 +0530

    hosting

--- hosting/hosting.js
+++ hosting/hosting.js
@@ -1,9 +1,31 @@
 console.log("hi");
 console.log("Hello");
 console.log("bye");
+console.log(a);
+
 
 var a=10;
+console.log(a);
+
 var b=20;
 var c=30;
+
+
+let d=40;
+console.log(d);
+
+
+
+const e=50;
+console.log(e);
+
+abc();
+function abc()
+{
+    console.log("HI iam abc function");
+    
+}
+
+abc();
 
 
