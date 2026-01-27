55caf3bd738e8495b619ba325e1fe0aac4fcac3b
dharunkumar <kumardharun607@gmail.com>
2026-01-27 14:56:48 +0530

    Object

---
+++ DMS/dms.js
@@ -0,0 +1,23 @@
+let a=true;
+let b=false;
+
+let c=Boolean(0);
+let d=Boolean(1);
+let e=Boolean(NaN);
+let f=Boolean(false);
+let g=Boolean(undefined);
+let h=Boolean(null);
+let i=Boolean("");
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+
+
+
